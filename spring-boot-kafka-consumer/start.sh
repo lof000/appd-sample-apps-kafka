e04192620600dfdac96b85b63021da58b60abf01
Leandro Ferreira <leandro.ferreira@gmail.com>
2023-03-01 21:29:29 -0300

    update start stripts

--- spring-boot-kafka-consumer/start.sh
+++ spring-boot-kafka-consumer/start.sh
@@ -1,1 +1,4 @@
-java -Dappdynamics.agent.tierName="consumer" -Dappdynamics.agent.nodeName="consumer_node" -javaagent:/Users/ledeoliv/WORK/LABS/KAFKA/agent/javaagent.jar  -jar target/spring-kafka-consumer-0.0.1-SNAPSHOT.jar
+
+java -jar target/spring-kafka-consumer-0.0.1-SNAPSHOT.jar
+
+#java -Dappdynamics.agent.tierName="consumer" -Dappdynamics.agent.nodeName="consumer_node" -javaagent:$AGENT_PATH/javaagent.jar  -jar target/spring-kafka-consumer-0.0.1-SNAPSHOT.jar
--- spring-boot-kafka-producer/start.sh
+++ spring-boot-kafka-producer/start.sh
@@ -1,1 +1,5 @@
-java -Dappdynamics.agent.tierName="producer" -Dappdynamics.agent.nodeName="producer_node" -javaagent:/Users/ledeoliv/WORK/LABS/KAFKA/agent/javaagent.jar  -jar target/spring-kafka-producer-0.0.1-SNAPSHOT.jar
+
+#java -Dappdynamics.agent.tierName="producer" -Dappdynamics.agent.nodeName="producer_node" -javaagent:$AGENT_PATH/javaagent.jar  -jar target/spring-kafka-producer-0.0.1-SNAPSHOT.jar
+
+
+java  -jar target/spring-kafka-producer-0.0.1-SNAPSHOT.jar
